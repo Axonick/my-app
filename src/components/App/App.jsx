1a21f33fde87b3f55e6a07254cfcf4c33da04e26
Roman Sinelnikov <Axonic@mail.ru>
2025-08-04 18:53:43 +0300

    Add files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { MyName } from '../MyName/MyName.jsx'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1>Привет React!</h1>
+        <p>Это мой первый React-проект с Vite.</p>
+        <MyName />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MyName/MyName.jsx
@@ -0,0 +1,5 @@
+import './MyName.css'
+
+export function MyName() {
+  return <h2>Меня зовут Роман!</h2>
+}
